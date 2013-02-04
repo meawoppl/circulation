9d3262d094e2a346c3c5676b5fcb9835878ab06e
o8ruza8o <ruza.markov@gmail.com>
2013-02-04 01:35:33 -0800

    type error fixed

--- circles.py
+++ circles.py
@@ -17,19 +17,18 @@ def radii(p1, p2, p3):
     return [r1] + p1, [r2] + p2, [r3] + p3
 
 def center(c1, c2, c3, r):
-    return type(c1), type(c2),  type(c3), type(r)
     
     b1 = 2*(c2[1]-c1[1])
-    c1 = 2*(c2[2]-c1[2])
+    a1 = 2*(c2[2]-c1[2])
     d1 = (c1[0] + r)**2 - (c2[0] + r)**2 + c2[1]**2  - c1[1]**2 + c2[2]**2  - c1[2]**2
     
     b3 = 2*(c2[1]-c3[1])
-    c3 = 2*(c2[2]-c3[2])
+    a3 = 2*(c2[2]-c3[2])
     d3 = (c3[0] + r)**2 - (c2[0] + r)**2 + c2[1]**2  - c3[1]**2 + c2[2]**2  - c3[2]**2
 
-    Cx = -(c3*d1 - c1*d3)/(b3*c1 - b1*c3)
-    Cy =  (b3*d1 - b1*d3)/(b3*c1 - b1*c3)
-    return Cx, Cy
+    Cx = -(a3*d1 - a1*d3)/(b3*a1 - b1*a3)
+    Cy =  (b3*d1 - b1*d3)/(b3*a1 - b1*a3)
+    return [Cx, Cy]
 
 
 def circit(c1, c2, c3):
@@ -44,7 +43,7 @@ def circit(c1, c2, c3):
     rI = r1*r2*r3/(r1*r2 + r1*r3 + r2*r3 + 2*sqrt(r1*r2*r3*(r1 + r2 + r3)))
     rO = r1*r2*r3/(r1*r2 + r1*r3 + r2*r3 - 2*sqrt(r1*r2*r3*(r1 + r2 + r3)))
 
-    return [rI, center(c1, c2, c3, rI)], [rO, center(c1, c2, c3, rO)]
+    return [rI] +  center(c1, c2, c3, rI), [rO] + center(c1, c2, c3, rO)
 
 def circIn(c1, c2, c3):
     """Given three circles returns inner circle only
@@ -56,7 +55,7 @@ def circIn(c1, c2, c3):
     
     rI = r1*r2*r3/(r1*r2 + r1*r3 + r2*r3 + 2*sqrt(r1*r2*r3*(r1 + r2 + r3)))
 
-    return [rI, center(c1, c2, c3, rI)]
+    return [rI] + center(c1, c2, c3, rI)
 
 def circOut(c1, c2, c3):
     """Given three circles returns outter circle only
@@ -68,4 +67,4 @@ def circOut(c1, c2, c3):
 
     rO = r1*r2*r3/(r1*r2 + r1*r3 + r2*r3 - 2*sqrt(r1*r2*r3*(r1 + r2 + r3)))
 
-    return [rO, center(c1, c2, c3, rO)]
+    return [rO] + center(c1, c2, c3, rO)
